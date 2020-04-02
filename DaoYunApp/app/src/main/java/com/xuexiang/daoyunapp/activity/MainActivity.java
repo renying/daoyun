fbc3af3b1da2dffb52fe6c58637cf91d2de15806
zjwml4581792 <814464214@qq.com>
2020-04-02 21:09:38 +0800

    去掉模版的侧边栏

---
+++ DaoYunApp/app/src/main/java/com/xuexiang/daoyunapp/activity/MainActivity.java
@@ -0,0 +1,207 @@
+/*
+ * Copyright (C) 2019 xuexiangjys(xuexiangjys@163.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.xuexiang.daoyunapp.activity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+import com.xuexiang.daoyunapp.R;
+import com.xuexiang.daoyunapp.core.BaseActivity;
+import com.xuexiang.daoyunapp.core.BaseFragment;
+import com.xuexiang.daoyunapp.fragment.AboutFragment;
+import com.xuexiang.daoyunapp.fragment.SettingsFragment;
+import com.xuexiang.daoyunapp.fragment.news.NewsFragment;
+import com.xuexiang.daoyunapp.fragment.profile.ProfileFragment;
+import com.xuexiang.daoyunapp.fragment.trending.TrendingFragment;
+import com.xuexiang.daoyunapp.utils.Utils;
+import com.xuexiang.daoyunapp.utils.XToastUtils;
+import com.xuexiang.xaop.annotation.SingleClick;
+import com.xuexiang.xui.adapter.FragmentAdapter;
+import com.xuexiang.xui.utils.ResUtils;
+import com.xuexiang.xui.widget.imageview.RadiusImageView;
+import com.xuexiang.xutil.XUtil;
+import com.xuexiang.xutil.common.ClickUtils;
+import com.xuexiang.xutil.common.CollectionUtils;
+
+import butterknife.BindView;
+
+/**
+ * 程序主页面,只是一个简单的Tab例子
+ *
+ * @author xuexiang
+ * @since 2019-07-07 23:53
+ */
+public class MainActivity extends BaseActivity implements View.OnClickListener, ViewPager.OnPageChangeListener, BottomNavigationView.OnNavigationItemSelectedListener, ClickUtils.OnClick2ExitListener, Toolbar.OnMenuItemClickListener {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.view_pager)
+    ViewPager viewPager;
+    /**
+     * 底部导航栏
+     */
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView bottomNavigation;
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
+    private String[] mTitles;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initViews();
+
+        initListeners();
+    }
+
+    @Override
+    protected boolean isSupportSlideBack() {
+        return false;
+    }
+
+    private void initViews() {
+        mTitles = ResUtils.getStringArray(R.array.home_titles);
+        toolbar.setTitle(mTitles[0]);
+        toolbar.inflateMenu(R.menu.menu_main);
+        toolbar.setOnMenuItemClickListener(this);
+
+
+        //主页内容填充
+        BaseFragment[] fragments = new BaseFragment[]{
+                new NewsFragment(),
+                new TrendingFragment(),
+                new ProfileFragment()
+        };
+        FragmentAdapter<BaseFragment> adapter = new FragmentAdapter<>(getSupportFragmentManager(), fragments);
+        viewPager.setOffscreenPageLimit(mTitles.length - 1);
+        viewPager.setAdapter(adapter);
+    }
+    protected void initListeners() {
+
+        //主页事件监听
+        viewPager.addOnPageChangeListener(this);
+        bottomNavigation.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_privacy:
+                Utils.showPrivacyDialog(this, null);
+                break;
+            default:
+                break;
+        }
+        return false;
+    }
+
+    @SingleClick
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.nav_header:
+                XToastUtils.toast("点击头部！");
+                break;
+            default:
+                break;
+        }
+    }
+
+    //=============ViewPager===================//
+
+    @Override
+    public void onPageScrolled(int i, float v, int i1) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        MenuItem item = bottomNavigation.getMenu().getItem(position);
+        toolbar.setTitle(item.getTitle());
+        item.setChecked(true);
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int i) {
+
+    }
+
+    //================Navigation================//
+
+    /**
+     * 底部导航栏点击事件
+     *
+     * @param menuItem
+     * @return
+     */
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int index = CollectionUtils.arrayIndexOf(mTitles, menuItem.getTitle());
+        if (index != -1) {
+            toolbar.setTitle(menuItem.getTitle());
+            viewPager.setCurrentItem(index, false);
+
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 菜单、返回键响应
+     */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            ClickUtils.exitBy2Click(2000, this);
+        }
+        return true;
+    }
+
+    @Override
+    public void onRetry() {
+        XToastUtils.toast("再按一次退出程序");
+    }
+
+    @Override
+    public void onExit() {
+        XUtil.get().exitApp();
+    }
+
+
+}
