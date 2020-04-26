a1a5224ec825dbac376f5ac257f88a85c49f5e2a
renyingwolf <616292525@qq.com>
2020-04-26 21:22:19 +0800

    change api

--- WebBase/controllers/ApiController.php
+++ WebBase/controllers/ApiController.php
@@ -1,22 +1,50 @@
 <?php
 namespace app\controllers;
+use Yii;
 use yii\web\Controller;
+use app\models\User;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
 class ApiController extends Controller
 {
   public $enableCsrfValidation = false;
   public function actionUserLogin()
   {
     $request = \Yii::$app->request;
-    echo('here');
-
+    $result=array(
+        'code'=>9999,
+        'message'=>'system error',
+        'data'=>null,
+    );
     if ($request->isPost) {
         $username = $request->post('u');
-        $password = $request->post('p');
-        if('team28'==$username&&'pd'==$password){
-        echo('ok');
+        $password = md5( $request->post('p'));
+
+        $userModel= new User();
+        $userList = $userModel::find()->asArray()->all();
+        $isSuccess=false;
+        $userId='';
+        foreach($userList as $user){
+            if($user['UserName']==$username && $user['PassWord']==$password){
+                $isSuccess=true;
+                $userId=$user['UserId'];
+                break;
+            }
+        }
+        if($isSuccess){
+            $result=array(
+              'code'=>1,
+              'message'=>'true',
+              'data'=>$userId,
+            );
         }
         else{
-        echo('error');
+            $result=array(
+              'code'=>1002,
+              'message'=>'username or password is wrong',
+              'data'=>null,
+            );
         }
     }
     else{
@@ -25,10 +53,8 @@ class ApiController extends Controller
           'message'=>'please post the request',
           'data'=>null,
         );
-        //输出json
-        echo json_encode($result);
     }
-
+    echo json_encode($result);
 
   }
 }
