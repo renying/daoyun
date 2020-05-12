3c5c6148bac4be8b9f185c684a5ad93fd6ef8ffa
zjwml4581792 <814464214@qq.com>
2020-05-13 01:11:51 +0800

    通信

--- DaoYunApp/app/src/main/java/com/team28/daoyunapp/fragment/LoginFragment.java
+++ DaoYunApp/app/src/main/java/com/team28/daoyunapp/fragment/LoginFragment.java
@@ -3,13 +3,18 @@ package com.team28.daoyunapp.fragment;
 import android.graphics.Color;
 import android.view.View;
 
+import com.team28.daoyunapp.core.http.Api;
+import com.team28.daoyunapp.core.http.CustomApiResult;
+import com.team28.daoyunapp.core.http.callback.TipCallBack;
 import com.xuexiang.xaop.annotation.SingleClick;
+import com.xuexiang.xhttp2.XHttp;
+import com.xuexiang.xhttp2.callback.CallBackProxy;
+import com.xuexiang.xhttp2.exception.ApiException;
 import com.xuexiang.xpage.annotation.Page;
 import com.xuexiang.xpage.enums.CoreAnim;
 import com.xuexiang.xui.utils.CountDownButtonHelper;
 import com.xuexiang.xui.utils.ResUtils;
 import com.xuexiang.xui.widget.actionbar.TitleBar;
-import com.xuexiang.xui.widget.button.roundbutton.RoundButton;
 import com.xuexiang.xui.widget.edittext.materialedittext.MaterialEditText;
 import com.team28.daoyunapp.R;
 import com.team28.daoyunapp.activity.MainActivity;
@@ -19,7 +24,9 @@ import com.team28.daoyunapp.utils.SettingSPUtils;
 import com.team28.daoyunapp.utils.TokenUtils;
 import com.team28.daoyunapp.utils.XToastUtils;
 import com.xuexiang.xutil.app.ActivityUtils;
-import com.xuexiang.xutil.common.RandomUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -34,107 +41,133 @@ import butterknife.OnClick;
 @Page(anim = CoreAnim.none)
 public class LoginFragment extends BaseFragment {
 
-    @BindView(R.id.et_phone_number)
-    MaterialEditText etPhoneNumber;
-    @BindView(R.id.et_verify_code)
-    MaterialEditText etVerifyCode;
-    @BindView(R.id.btn_get_verify_code)
-    RoundButton btnGetVerifyCode;
+    @BindView(R.id.et_account)
+    MaterialEditText etAccount;
+    @BindView(R.id.et_password)
+    MaterialEditText etPassword;
 
     private CountDownButtonHelper mCountDownHelper;
 
     @Override
-    protected int getLayoutId() {
+    protected int getLayoutId () {
         return R.layout.fragment_login;
     }
 
     @Override
-    protected TitleBar initTitle() {
-        TitleBar titleBar = super.initTitle()
-                .setImmersive(true);
-        titleBar.setBackgroundColor(Color.TRANSPARENT);
-        titleBar.setTitle("");
-        titleBar.setLeftImageDrawable(ResUtils.getVectorDrawable(getContext(), R.drawable.ic_login_close));
+    protected TitleBar initTitle () {
+        TitleBar titleBar = super.initTitle ()
+                .setImmersive (true);
+        titleBar.setBackgroundColor (Color.TRANSPARENT);
+        titleBar.setTitle ("");
+        titleBar.setLeftImageDrawable (ResUtils.getVectorDrawable (getContext (), R.drawable.ic_login_close));
         return titleBar;
     }
 
     @Override
-    protected void initViews() {
-        mCountDownHelper = new CountDownButtonHelper (btnGetVerifyCode, 60);
+    protected void initViews () {
 
         //隐私政策弹窗
-        SettingSPUtils spUtils = SettingSPUtils.getInstance();
-        if (!spUtils.isAgreePrivacy()) {
-            PrivacyUtils.showPrivacyDialog(getContext(), (dialog, which) -> {
-                dialog.dismiss();
-                spUtils.setIsAgreePrivacy(true);
+        SettingSPUtils spUtils = SettingSPUtils.getInstance ();
+        if (! spUtils.isAgreePrivacy ()) {
+            PrivacyUtils.showPrivacyDialog (getContext (), ( dialog, which ) -> {
+                dialog.dismiss ();
+                spUtils.setIsAgreePrivacy (true);
             });
         }
     }
 
     @SingleClick
-    @OnClick({R.id.btn_get_verify_code, R.id.btn_login, R.id.tv_other_login, R.id.tv_forget_password, R.id.tv_user_protocol, R.id.tv_privacy_protocol})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.btn_get_verify_code:
-                if (etPhoneNumber.validate()) {
-                    getVerifyCode(etPhoneNumber.getEditValue());
-                }
-                break;
+    @OnClick({R.id.btn_login, R.id.tv_register, R.id.tv_forget_password, R.id.tv_user_protocol, R.id.tv_privacy_protocol})
+    public void onViewClicked ( View view ) {
+        switch (view.getId ()) {
             case R.id.btn_login:
-                if (etPhoneNumber.validate()) {
-                    if (etVerifyCode.validate()) {
-                        loginByVerifyCode(etPhoneNumber.getEditValue(), etVerifyCode.getEditValue());
+                if (etAccount.validate ()) {
+                    if (etPassword.validate ()) {
+                        loginByPassword (etAccount.getEditValue (), etPassword.getEditValue ());
                     }
                 }
                 break;
-            case R.id.tv_other_login:
-                XToastUtils.info("其他登录方式");
+            case R.id.tv_register:
+                XToastUtils.info ("注册");
                 break;
             case R.id.tv_forget_password:
-                XToastUtils.info("忘记密码");
+                XToastUtils.info ("忘记密码");
                 break;
             case R.id.tv_user_protocol:
-                XToastUtils.info("用户协议");
+                XToastUtils.info ("用户协议");
                 break;
             case R.id.tv_privacy_protocol:
-                XToastUtils.info("隐私政策");
+                XToastUtils.info ("隐私政策");
                 break;
             default:
                 break;
         }
     }
 
-    /**
-     * 获取验证码
-     */
-    private void getVerifyCode(String phoneNumber) {
-        // TODO: 2019-11-18 这里只是界面演示而已
-        XToastUtils.warning("只是演示，验证码请随便输");
-        mCountDownHelper.start();
-    }
+//    /**
+//     * 获取验证码
+//     */
+//    private void getVerifyCode ( String phoneNumber ) {
+//        // TODO: 2019-11-18 这里只是界面演示而已
+//        XToastUtils.warning ("只是演示，验证码请随便输");
+//        mCountDownHelper.start ();
+//    }
+//
+//    /**
+//     * 根据验证码登录
+//     *
+//     * @param phoneNumber 手机号
+//     * @param verifyCode  验证码
+//     */
+//    private void loginByVerifyCode ( String phoneNumber, String verifyCode ) {
+//        // TODO: 2019-11-18 这里只是界面演示而已
+//        String token = RandomUtils.getRandomNumbersAndLetters (16);
+//        if (TokenUtils.handleLoginSuccess (token)) {
+//            popToBack ();
+//            ActivityUtils.startActivity (MainActivity.class);
+//        }
+//    }
 
-    /**
-     * 根据验证码登录
-     *
-     * @param phoneNumber 手机号
-     * @param verifyCode  验证码
-     */
-    private void loginByVerifyCode(String phoneNumber, String verifyCode) {
-        // TODO: 2019-11-18 这里只是界面演示而已
-        String token = RandomUtils.getRandomNumbersAndLetters(16);
-        if (TokenUtils.handleLoginSuccess(token)) {
-            popToBack();
-            ActivityUtils.startActivity(MainActivity.class);
+    private void loginByPassword ( String account, String password ) {
+        //获取当前时间
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault());// HH:mm:ss//获取当前时间
+//        Date date = new Date(System.currentTimeMillis());
+//        String timeStamp = "Date获取当前日期时间"+simpleDateFormat.format(date);
+        //通信
+        JSONObject loginJson = new JSONObject ();
+        try {
+            loginJson.put ("u",account);
+            loginJson.put ("p",password);
+        } catch (JSONException e) {
+            e.printStackTrace ();
         }
-    }
+//        String loginJson = "{\"u\":\"" + account + "\",\"p\":\"" + password + "\"}";
+        XHttp.post (Api.LOGIN)
+                .upJson (loginJson.toString ())
+                .execute (new CallBackProxy<CustomApiResult<Boolean>, Boolean> (new TipCallBack<Boolean> () {
+                    @Override
+                    public void onSuccess ( Boolean response ) throws Throwable {
+                        if (TokenUtils.handleLoginSuccess (account)) {
+                            popToBack ();
+                            ActivityUtils.startActivity (MainActivity.class);
+                        }
+                    }
 
+                    @Override
+                    public void onError ( ApiException e ) {
+                        super.onError (e);
+                    }
+
+                }) {
+                });
+
+    }
 
     @Override
-    public void onDestroyView() {
+    public void onDestroyView () {
         if (mCountDownHelper != null) {
-            mCountDownHelper.recycle();
+            mCountDownHelper.recycle ();
         }
-        super.onDestroyView();
+        super.onDestroyView ();
     }
 }
