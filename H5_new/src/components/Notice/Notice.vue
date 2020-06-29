d107f6336448e6184443eed5051de3fa0ee318bc
Annie333 <949040872@qq.com>
2020-06-29 23:26:20 +0800

    noticeupdate

--- H5_new/src/components/Notice/Notice.vue
+++ H5_new/src/components/Notice/Notice.vue
@@ -31,6 +31,7 @@
     </el-header>
     <el-main>
     <el-tabs type="border-card">
+      <!-- --------------------------这里是班课通知------------------------------------- -->
       <el-tab-pane label="班课通知">
         <div class="wrapper">
             <div class="container-fluid">
@@ -47,29 +48,18 @@
                                     </div>
                                 </a>
 
-                                <!-- <a href="#" class="media">
-                                    <img class="d-flex mr-3 rounded-circle" src="@/assets/images/users/avatar-3.jpg" alt="Generic placeholder image" height="36">
-                                    <div class="media-body chat-user-box">
-                                      <p class="user-title m-0">工程实践</p>
-                                      <p class="text-muted"></p>
-                                    </div>
-                                </a>
-
-                                <a href="#" class="media">
-                                    <img class="d-flex mr-3 rounded-circle" src="@/assets/images/users/avatar-4.jpg" alt="Generic placeholder image" height="36">
-                                    <div class="media-body chat-user-box">
-                                      <p class="user-title m-0">软件体系结构</p>
-                                      <p class="text-muted"></p>
-                                    </div>
-                                </a> -->
                             </div>
                   </div>
                   <div class="email-rightbar mb-3">
                     <div class="card m-t-20">
                       <ul class="message-list" v-for="(item, index) in NoticeList" v-bind:key="index">
                         <li>
-                                        <a href="">
+                                        <a>
                                             <div class="col-mail col-mail-1">
+                                              <div class="checkbox-wrapper-mail">
+                                                    <input type="checkbox" id="chk19">
+                                                    <label for="chk19" class="toggle"></label>
+                                                </div>
                                                 <p class="title">{{item.FromName}}</p><span class="star-toggle fa fa-star-o"></span>
                                             </div>
                                             <div class="col-mail col-mail-2">
@@ -77,6 +67,34 @@
                                                 </div>
                                                 <div class="date">{{item.InfoDate}}</div>
                                             </div>
+                                          <div class="col-mail col-mail-3">
+                                                <!--
+                                                *<el-button type="primary" @click="dialogVisible = true">阅读详情</el-button>
+                                                -->
+                                            <el-button type="primary" @click=readMessage()>阅读详情</el-button>
+                                            <!--这个方法试了半天，不知道该怎么写，需要在click时，显示新窗口，同时发送数据，方法定义在305行-->
+                                          <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
+                                            <span>这是一段信息</span>
+                                            <div class="card m-t-20">
+                                              <div class="card-body">
+                                                <div class="media m-b-30">
+                                                  <img class="d-flex mr-3 rounded-circle thumb-md" src="@/assets/images/users/avatar-1.jpg" alt="Generic placeholder image">
+                                                  <div class="media-body">
+                                                    <h4 class="font-14 m-0">这里是发消息的用户FromName</h4>
+                                                    <small class="text-muted">这里是发消息的时间InfoDate</small>
+                                                  </div>
+                                                </div>
+                                                <h4 class="mt-0 font-18">这里是发消息的用户FromName</h4>
+                                                <p>这里是InfoContent</p>
+                                                <hr>
+                                              </div>
+                                            </div>
+                                            <span slot="footer" class="dialog-footer">
+                                              <el-button @click="dialogVisible = false">取 消</el-button>
+                                              <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+                                            </span>
+                                          </el-dialog>
+                                          </div>
                                         </a>
                         </li>
                         <!-- <li>
@@ -99,8 +117,91 @@
             </div>
         </div>
       </el-tab-pane>
+      <!-- --------------------------------这里是用户消息-------------------------------------------- -->
       <el-tab-pane label="用户消息">
         这里是用户消息
+        <div class="wrapper">
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-12">
+                  <div class="email-leftbar">
+                    <div class="m-t-20" v-for="(item, index) in NoticeList" v-bind:key="index">
+                                <a href="#" class="media">
+                                    <img class="d-flex mr-3 rounded-circle" src="@/assets/images/users/avatar-2.jpg" alt="Generic placeholder image" height="36">
+                                    <div class="media-body chat-user-box">
+                                      <el-badge :is-dot="item.HasNew" class="item">{{item.FromName}}</el-badge>
+                                      <p class="user-title m-0">{{formatType(item.NoticeType)}}</p>
+                                      <p class="text-muted"></p>
+                                    </div>
+                                </a>
+                            </div>
+                  </div>
+                  <div class="email-rightbar mb-3">
+                    <div class="card m-t-20">
+                      <ul class="message-list" v-for="(item, index) in NoticeList" v-bind:key="index">
+                        <li>
+                                        <a>
+                                            <div class="col-mail col-mail-1">
+                                              <div class="checkbox-wrapper-mail">
+                                                    <input type="checkbox" id="chk20">
+                                                    <label for="chk20" class="toggle"></label>
+                                                </div>
+                                                <p class="title">{{item.FromName}}</p><span class="star-toggle fa fa-star-o"></span>
+                                            </div>
+                                            <div class="col-mail col-mail-2">
+                                                <div class="subject"><span class="teaser">{{item.InfoContent}}</span>
+                                                </div>
+                                                <div class="date">{{item.InfoDate}}</div>
+                                            </div>
+                                          <div class="col-mail col-mail-3">
+                                                <!--
+                                                *<el-button type="primary" @click="dialogVisible = true">阅读详情</el-button>
+                                                -->
+                                            <el-button type="primary" @click=readMessage()>阅读详情</el-button>
+                                            <!--这个方法试了半天，不知道该怎么写，需要在click时，显示新窗口，同时发送数据，方法定义在305行-->
+                                          <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
+                                            <span>这是一段信息</span>
+                                            <div class="card m-t-20">
+                                              <div class="card-body">
+                                                <div class="media m-b-30">
+                                                  <img class="d-flex mr-3 rounded-circle thumb-md" src="@/assets/images/users/avatar-1.jpg" alt="Generic placeholder image">
+                                                  <div class="media-body">
+                                                    <h4 class="font-14 m-0">这里是发消息的用户FromName</h4>
+                                                    <small class="text-muted">这里是发消息的时间InfoDate</small>
+                                                  </div>
+                                                </div>
+                                                <h4 class="mt-0 font-18">这里是发消息的用户FromName</h4>
+                                                <p>这里是InfoContent</p>
+                                                <hr>
+                                              </div>
+                                            </div>
+                                            <span slot="footer" class="dialog-footer">
+                                              <el-button @click="dialogVisible = false">取 消</el-button>
+                                              <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+                                            </span>
+                                          </el-dialog>
+                                          </div>
+                                        </a>
+                        </li>
+                        <!-- <li>
+                                        <a href="">
+                                            <div class="col-mail col-mail-1">
+                                                <p class="title">池芝标</p><span class="star-toggle fa fa-star-o"></span>
+                                            </div>
+                                            <div class="col-mail col-mail-2">
+                                                <div class="subject"><span class="teaser">测试消息.</span>
+                                                </div>
+                                                <div class="date">Mar. 7</div>
+                                            </div>
+                                        </a>
+                        </li> -->
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
       </el-tab-pane>
     </el-tabs>
     </el-main>
@@ -129,10 +230,18 @@ export default {
       content: '',
       not_type: '',
       isShow: false,
+      dialogVisible: false,
       NoticeList: []
     }
   },
   methods: {
+    handleClose (done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          done()
+        })
+        .catch(_ => {})
+    },
     formatType (nottype) { // 表格数据转换 消息类别
       return nottype === '1' ? '班课通知' : nottype === '2' ? '用户消息' : '未知类型'
     },
@@ -169,7 +278,7 @@ export default {
       var t = this
       var myDate = new Date()
       var qs = require('qs')
-      this.$axios.post('api/get-usernoticelist', qs.stringify({
+      this.$axios.post('api/get-noticeinfolist', qs.stringify({
         ui: localStorage.getItem('userid'),
         ukey: localStorage.getItem('ukey'),
         TimeStamp: myDate
@@ -193,6 +302,8 @@ export default {
         .catch(function (error) {
           console.log(error)
         })
+    },
+    readMessage () {
     }
   },
   mounted () {
